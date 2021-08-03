09c6a9183e1d49354e8527b77bddaed0e5e5afa5
TDB-star <tatiana.blache@gmail.com>
2021-08-03 16:57:35 +0200

    Первые шаги: установила фреймворк, настроила сторибоард, первая модель (последовательный перебор доступных анимаций)

--- SpringAnimationApp/ViewController.swift
+++ SpringAnimationApp/ViewController.swift
@@ -5,15 +5,36 @@
 //  Created by Tatiana Dmitrieva on 03/08/2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var animationView: SpringView!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var runButton: SpringButton!
+    
+    
+    private let anim = Spring.AnimationPreset.allCases
+    let animationCurves = Spring.AnimationCurve.allCases
+    
+    private var currentIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        animationView.layer.cornerRadius = 12
     }
-
-
+    
+    @IBAction func runButtonPressed(_ sender: SpringButton) {
+        setAnimation()
+        animationView.animate()
+//        currentIndex += 1
+//        animationView.animation = animations[currentIndex % animations.count].rawValue
+//        animationView.animate()
+    }
+    
+    func setAnimation() {
+        currentIndex += 1
+        animationView.animation = anim[currentIndex].rawValue
+    }
 }
 
